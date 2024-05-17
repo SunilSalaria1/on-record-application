bc2ddd787a66ce7219ab9417f0b5cb053365938a
Sunil <sunil.salaria@lpinfotech.com>
2024-05-17 16:42:43 +0530

    fix signup form builder

--- src/app/pages/auth/signup/signup.component.ts
+++ src/app/pages/auth/signup/signup.component.ts
@@ -1,40 +1,55 @@
 import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
-import {MatSelectModule} from '@angular/material/select';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatNativeDateModule} from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import {FormBuilder} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+import {Validators} from '@angular/forms';
 
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule, MatInputModule, MatIconModule, MatDividerModule, RouterOutlet, RouterLink, 
-    RouterLink, RouterLinkActive, MatSelectModule, MatDatepickerModule, MatNativeDateModule, MatFormFieldModule],
+  imports: [MatButtonModule, MatCardModule, MatInputModule, MatIconModule, MatDividerModule, RouterOutlet, RouterLink,
+    RouterLink, RouterLinkActive, MatSelectModule, MatDatepickerModule, MatNativeDateModule, MatFormFieldModule, ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
-
+  constructor(private formBuilder: FormBuilder) {}
   //== labels 
   firstName = "First Name";
   lastName = "Last Name";
   emailAddress = "Email Address";
-  mobileNumber = "Mobile Number"; 
+  mobileNumber = "Mobile Number";
   selectGender = "Select Gender";
   selectGenderList = ['Male', 'Female', 'Others'];
-  selectRoleOptions = ['BDE', 'Backend Developer', 'Frontend Developer', 'Full Stack Developer', 'Human Resources'];
   password = "Password";
   confirmPassword = "Confirm Password";
   signUpBtn = "Sign Up";
 
+  //== signup formBuilder
+  signUpForm = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],   
+    emailAddress: ['',Validators.required],   
+    mobileNumber: ['', Validators.required],   
+    selectGender: ['', Validators.required],   
+    password: ['', Validators.required],   
+    confirmPassword: ['', Validators.required]   
+    });
 
-
+    //== onSubmit signup formBuilder
+    onSubmit(){
+      console.warn(this.signUpForm.value);
+    }
 }
