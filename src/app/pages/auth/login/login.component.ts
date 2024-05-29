7e56e2403c79350c7bb6486de42bdeb00ebb5c3e
Sunil <sunil.salaria@lpinfotech.com>
2024-05-29 12:10:28 +0530

    login page changes

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,20 +1,47 @@
 import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule, MatInputModule, MatIconModule, MatDividerModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [MatButtonModule, MatCardModule, MatInputModule, MatIconModule, MatDividerModule, RouterOutlet, RouterLink, RouterLinkActive, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  constructor(private formBuilder: FormBuilder) { }
 
+  //== form labels 
+  labels = {
+    emailAddress: "Email Address",
+    password: "Password",
+    signInBtn: "Sign In",
+    loginWithGoogle: "Continue with Google",
+    loginWithMobile: "Continue with mobile OTP"
+  }
+
+  //== password fields visible icon default state
+  passwordVisibleIcon = true;
+
+  //== sign in form builder
+  signInForm = this.formBuilder.group({
+    emailAddress: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  });
+
+  //== onSubmit signInForm formBuilder
+  onSubmit() {
+    if (this.signInForm.valid) {
+      console.warn(this.signInForm.value);
+    }
+  }
 }
