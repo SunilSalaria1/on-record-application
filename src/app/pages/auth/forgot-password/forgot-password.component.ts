a24ce2ca5906759d92eaa266e1e80fc3b30235fe
Sunil <sunil.salaria@lpinfotech.com>
2024-05-29 12:50:08 +0530

    forgot password page update

--- src/app/pages/auth/forgot-password/forgot-password.component.ts
+++ src/app/pages/auth/forgot-password/forgot-password.component.ts
@@ -9,15 +9,34 @@ import { RouterLink } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { SnackbarComponent } from '../../../shared/components/snackbar/snackbar.component';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule, MatInputModule, MatIconModule, MatDividerModule, RouterOutlet, RouterLink, RouterLinkActive, MatSnackBarModule, SnackbarComponent],
+  imports: [MatButtonModule, MatCardModule, MatInputModule, MatIconModule, MatDividerModule, RouterOutlet, RouterLink, RouterLinkActive, MatSnackBarModule, SnackbarComponent, ReactiveFormsModule],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.scss'
 })
 export class ForgotPasswordComponent {
-  constructor() {} 
+  constructor(private formBuilder: FormBuilder) { }
+
+  //== form labels 
+  labels = {
+    emailAddress: "Email Address"   
+  }
+
+  //== sign in form builder
+  forgotPasswordForm = this.formBuilder.group({
+    emailAddress: ['', [Validators.required, Validators.email]]
+  });
+
+  //== onSubmit signInForm formBuilder
+  onSubmit() {
+    if (this.forgotPasswordForm.valid) {
+      console.warn(this.forgotPasswordForm.value);
+    }
+  }
 }
