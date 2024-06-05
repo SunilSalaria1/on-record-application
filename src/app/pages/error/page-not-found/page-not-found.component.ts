36425bfca7c7e25ff35c775f2ba1b8e5080d6383
Sunil <sunil.salaria@lpinfotech.com>
2024-06-05 17:27:00 +0530

    page not found

--- src/app/pages/error/page-not-found/page-not-found.component.ts
+++ src/app/pages/error/page-not-found/page-not-found.component.ts
@@ -4,6 +4,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-page-not-found',
@@ -13,5 +14,17 @@ import {MatDividerModule} from '@angular/material/divider';
   styleUrl: './page-not-found.component.scss'
 })
 export class PageNotFoundComponent {
+  constructor(private location: Location) { }
 
+   //== labels 
+    goBackBtn = "Go Back";
+    ErrorTitle = "Opps! Page not Found.";
+    ErrorDescription = "The requested URL was not found on this server. The page you are looking for might have been removed, had its name changed or it's temporarily unavailable.";
+   
+ 
+
+  //== go back btn
+  goBack(): void {
+    this.location.back();
+  }
 }
