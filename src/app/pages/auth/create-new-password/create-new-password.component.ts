6f347be7d007988e460fd249d690082caceb1584
Sunil <sunil.salaria@lpinfotech.com>
2024-06-10 14:01:04 +0530

    create password update

--- src/app/pages/auth/create-new-password/create-new-password.component.ts
+++ src/app/pages/auth/create-new-password/create-new-password.component.ts
@@ -3,11 +3,12 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
+import { PasswordComponent } from "../../../shared/widgets/password-widget/password/password.component";
 
 @Component({
   selector: 'app-create-new-password',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule, MatInputModule, MatIconModule],
+  imports: [MatButtonModule, MatCardModule, MatInputModule, MatIconModule, PasswordComponent],
   templateUrl: './create-new-password.component.html',
   styleUrl: './create-new-password.component.scss'
 })
